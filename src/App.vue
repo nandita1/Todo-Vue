84600db0d1149c09bbdf38c540f8faafb233c0fc
nandita1 <nandita.kalita1@gmail.com>
2020-05-31 20:02:19 +0530

    my first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="outer">
+    <div class="container">
+      <h3>Yet Another Todo</h3>
+      <app-new-todo @todoAdded="newTodo"></app-new-todo>
+      <hr />
+      <app-todos :todos="todos" @todoDeleted="deleteTodo"></app-todos>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import NewTodo from "./components/NewTodo";
+import Todos from "./components/Todos";
 export default {
-  name: 'App',
+  data() {
+    return {
+      todos: []
+    };
+  },
+  methods: {
+    newTodo(todo) {
+      this.todos.push(todo);
+    },
+    deleteTodo(index) {
+      this.todos.splice(index, 1);
+    }
+  },
   components: {
-    HelloWorld
+    appNewTodo: NewTodo,
+    appTodos: Todos
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: "Roboto", sans-serif;
+}
+.outer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #fafafa;
+}
+.container {
+  width: 300px;
+  background-color: white;
+  box-shadow: 0px 5px 10px gray;
+  padding: 20px;
+}
+h3 {
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 10px 0;
+}
+hr {
+  margin: 20px 0;
+  height: 0.5px;
+  border-width: 0;
+  color: #ccc;
+  background-color: #ccc;
 }
 </style>
---
+++ src/components/Todos.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <ul v-if="todos.length > 0">
+      <li v-for="(todo,i) in todos" :key="i">
+        <div>{{todo}}</div>
+        <span>
+          <img src="../assets/dustbin.svg" @click="deleteTodo(i)" />
+        </span>
+      </li>
+    </ul>
+    <div class="noTodos" v-else>No Todos here yet!</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["todos"],
+  methods: {
+    deleteTodo(index) {
+      this.$emit("todoDeleted", index);
+    }
+  }
+};
+</script>
+
+<style>
+li {
+  list-style: none;
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0;
+}
+.noTodos {
+  text-align: center;
+}
+img {
+  width: 15px;
+  height: 20px;
+  cursor: pointer;
+}
+</style>
